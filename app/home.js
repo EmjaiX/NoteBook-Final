359bb78b0bf3c88f873c8ecf7928e7f84fd72236
Hai-Liang-Chen <1081918898@sjc.bz>
2021-05-01 22:24:19 -0700

    upper

---
+++ app/home.js
@@ -0,0 +1,14 @@
+var observableModule = require("tns-core-modules/data/observable");  // load observable module
+exports.onLoaded = function(args) {
+    var page = args.object;
+    var home = new observableModule.fromObject({
+        header: "Note Book",
+        footer: "CIS 288 2021"
+    });
+    page.bindingContext = home;
+}
+
+exports.onTap = function(args){
+    var frame = require("tns-core-modules/ui/frame").Frame;
+    frame.topmost().navigate("notes");
+}
